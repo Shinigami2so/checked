d621ae58bfbe0bc5157b3cd45f74b3c4bea3b6bd
Thuso Mokane <thusomokane@me.com>
2016-08-28 10:29:42 +0200

    Initial Commit

---
+++ CheckedTests/CheckedTests.swift
@@ -0,0 +1,36 @@
+//
+//  CheckedTests.swift
+//  CheckedTests
+//
+//  Created by Thuso Mokane on 28/08/16.
+//  Copyright © 2016 Thuso Mokane. All rights reserved.
+//
+
+import XCTest
+@testable import Checked
+
+class CheckedTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
