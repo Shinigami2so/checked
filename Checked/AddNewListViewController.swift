953660cbb1ed633a5bb6144e50a533c898f4ee02
Shinigami2so <mokanethuso@gmail.com>
2016-08-29 17:22:23 +0200

    Add new list

---
+++ Checked/AddNewListViewController.swift
@@ -0,0 +1,46 @@
+//
+//  AddNewListViewController.swift
+//  Checked
+//
+//  Created by Thuso Mokane on 29/08/16.
+//  Copyright © 2016 Thuso Mokane. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+class AddNewListViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var list_name_text_field: UITextField!
+    
+    @IBAction func save_new_list(sender: AnyObject) {
+        addNewList(list_name_text_field.text!)
+    }
+    
+    
+    /*
+    *
+    */
+    func addNewList(list_name: String){
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedObjectContext : NSManagedObjectContext = appDelegate.managedObjectContext
+        
+        let newList = NSEntityDescription.insertNewObjectForEntityForName("List", inManagedObjectContext: managedObjectContext) as! List
+        
+        newList.name = list_name
+        newList.date_created = NSDate()
+        
+        do{
+            try managedObjectContext.save()
+            self.navigationController?.popViewControllerAnimated(false)
+        } catch {
+            print(error)
+        }
+        
+    }
+    
+    
+    
+    
+}
--- Checked/ListsViewController.swift
+++ Checked/ListsViewController.swift
@@ -7,3 +7,66 @@
 //
 
 import Foundation
+import CoreData
+import UIKit
+
+
+class ListsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var table: UITableView!
+    
+    var fetchedResultsController: NSFetchedResultsController!
+    
+    func initializeFetchedResultsController() {
+        let request = NSFetchRequest(entityName: "List")
+        let name_sort = NSSortDescriptor(key: "name", ascending: true)
+        let date_sort = NSSortDescriptor(key: "date_created", ascending: true)
+        request.sortDescriptors = [name_sort, date_sort]
+        
+        var managedObjectContext: NSManagedObjectContext?{
+            return (UIApplication.sharedApplication().delegate
+                as! AppDelegate).managedObjectContext
+        }
+        
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: request,
+                                   managedObjectContext: managedObjectContext!,
+                                   sectionNameKeyPath: nil,
+                                   cacheName: nil)
+        
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalError("Failed to initialize FetchedResultsController: \(error)")
+        }
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initializeFetchedResultsController()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        initializeFetchedResultsController()
+        table.reloadData()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        let sectionInfo = self.fetchedResultsController.sections![section]
+        
+        return sectionInfo.numberOfObjects
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCellWithIdentifier("list_cell", forIndexPath: indexPath) as UITableViewCell
+        
+        let list = fetchedResultsController.objectAtIndexPath(indexPath) as! List
+        
+        cell.textLabel?.text = list.name
+        
+        return cell
+    }
+    
+}
